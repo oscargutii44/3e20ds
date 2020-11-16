f1b3710171afc6095c54dc030a8319131b0a5c63
oscar <gutierrez_oscar@ucol.mx>
2020-11-16 16:45:11 -0600

    Subir ejercicio 1 de las series

---
+++ 1p/ej/series/series1.js
@@ -0,0 +1,15 @@
+const sen = (x) => { return (x - Math.pow(x, .5))};
+
+const iterador = () => { 
+    for(var i=0; i < 10; i++){
+        console.log('sen(', i, ') = ', sen(i))
+    } 
+}
+
+
+
+
+
+
+
+iterador()
